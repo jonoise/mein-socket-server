23692023c7961ba5701fb98160a1f0d149398724
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-21 23:17:03 -0600

    add two handlers

--- src/events/initEvents.js
+++ src/events/initEvents.js
@@ -1,9 +1,9 @@
 module.exports = (io, socket, db) => {
     // DB MODELS
-    let tables = db.get("tables")
 
     // INIT TABLE HANDLER
     const initTable = (rest_uuid, instance_uuid, tableNumber) => {
+        let tables = db.get("tables")
 
         socket.data.instance_uuid = instance_uuid
         socket.join(instance_uuid)
@@ -21,7 +21,6 @@ module.exports = (io, socket, db) => {
             }
 
             tables[instance_uuid] = newTable
-            console.log(tables)
             db.set("tables", tables)
             existingTable = newTable
         }
@@ -30,9 +29,28 @@ module.exports = (io, socket, db) => {
 
     // HYDRATE TABLE HANDLER
     const hydrateState = () => {
-        socket.emit("")
+        let tables = db.get("tables")
+        const table = tables[socket.data.instance_uuid]
+        socket.emit("init:hydration", table)
+    }
+
+    // DISCONNECT HANDLER
+    const disconnect = () => {
+        let tables = db.get("tables")
+        const table = tables[socket.data.instance_uuid]
+        if (table) {
+
+
+            table.usersIds = table.usersIds.filter(id => id !== socket.id)
+            delete table.users[socket.id]
+            db.set("tables", {
+                ...tables,
+                [table.id]: table
+            })
+        }
     }
 
     socket.on("init:table", initTable);
     socket.on("init:hydration", hydrateState);
+    socket.on("disconnect", disconnect)
 }
